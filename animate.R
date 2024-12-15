8f1f315ca70f7336979df0341efef7b5d812af9e
Heini Järviö <heinimjarv@gmail.com>
2024-12-15 15:00:27 +0100

    added animation

--- animate.R
+++ animate.R
@@ -1,15 +1,17 @@
-install.packages("gifski")
 library(gganimate)
 library(gifski)
 
-anim_plot <- ggplot(df_parental_leave_clean, aes(maternityleave_length, paternityleave_length, colour = country)) +
-    geom_point(size = 3, alpha = 0.7, show.legend = FALSE) +
-    scale_colour_manual(values = country_colors) +
+# Loading the cleaned data ---------------------------------------------------------
+
+df_wbl <- read_rds("parental_leave_clean.rds")
+
+anim_plot <- ggplot(df_wbl, aes(maternityleave_length, paternityleave_length, colour = country)) +
+    geom_point(size = 2, alpha = 0.7, show.legend = FALSE) +
+    # scale_colour_manual(values = country_colors) +
     scale_size(range = c(2, 60)) +
     facet_wrap(~region) +
-    # Here comes the gganimate specific bits
     shadow_trail(max_frames = 5) +
-    labs(title = 'Year: {frame_time}', x = 'length of maternity leave', y = 'length of paternity leave') +
+    labs(title = 'Year: {frame_time}', x = 'Length of maternity leave', y = 'Length of paternity leave') +
     transition_time(as.integer(year)) +
     ease_aes('linear') +
     xlim(0, 200) + 
@@ -21,6 +23,6 @@ anim_save("parental_leave2.gif", animation = last_animation(), path = NULL)
 
 ?ease_aes
 
-?transition
+?scale_color_manual
 
     
