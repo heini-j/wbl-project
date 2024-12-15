7e7adc5a388b3315a2122840f0bb3a9ac5e54566
Heini Järviö <heinimjarv@gmail.com>
2024-12-15 11:20:25 +0100

    added some analysis

--- analyse.R
+++ analyse.R
@@ -19,20 +19,20 @@ df_parental_leave_clean <- df_parental_leave |>
         maternityleave_minimum = "Is paid leave of at least 14 weeks available to mothers?",
         maternityleave_log = "Score...9",
         maternity_law = "Legal Basis...10",
-        maternityleave_lenght = "Length of paid maternity leave",
+        maternityleave_length = "Length of paid maternity leave",
         maternityleave_benefits = "Does the government administer 100 percent of maternity leave benefits?",
         maternityleave_benefits_log = "Score...13",
         maternitityleave_benefits_law = "Legal Basis...14",
         paternity_leave = "Is there paid leave available to fathers?",
         paternityleave_log = "Score...16",
         paternity_law = "Legal Basis...17",
-        paternityleave_lenght = "Length of paid paternity leave",
+        paternityleave_length = "Length of paid paternity leave",
         parentalleave = "Is there paid parental leave?",
         parentalleave_log = "Score...20",
         parentalleave_law = "Legal Basis...21",
-        shared_lenght = "Shared days",
-        shared_lenght_mother = "Days for the mother",
-        shared_lenght_father = "Days for the father",
+        shared_length = "Shared days",
+        shared_length_mother = "Days for the mother",
+        shared_length_father = "Days for the father",
         dismissal_prohibited = "Is dismissal of pregnant workers prohibited?",
         dismissal_prohibited_log = "Score...26",
         dismissal_prohibited_law = "Legal Basis...27"
@@ -61,43 +61,80 @@ max(df_parental_leave_clean$year)
 df_parental_leave_clean |>
     filter(year == 2000) |>
     summarise(
-    "mean_paternityleave" = mean(paternityleave_lenght),
-    "mean_maternityleave" = mean(maternityleave_lenght)
+    "mean_paternityleave" = mean(paternityleave_length),
+    "mean_maternityleave" = mean(maternityleave_length)
     )
 
-# Analysing decriptively -----------------------------------------------------------
+# Analysing descriptively -----------------------------------------------------------
+
+# Are there missing values in the data?
+
+sum(is.na(df_parental_leave_clean))
 
 # How many countries have paternity and maternity leave in each year?
 
 df_parental_leave_clean |>
-    filter(year == 2000) |>
+    filter(year == 2024) |>
     summarise(
     "countries_with_paternityleave" = sum(paternityleave_log == 1),
     "countries_with_maternityleave" = sum(maternityleave_log == 1)
     )
 
+# 35 and 77 in year 2000, 22 and 61 in year 1985, 123 and 123 in year 2024
+
 # Are there differences between income groups in the average 
 # length of paternity and maternity leave in the years after 2015?
 
-summary_leave_lenght <- df_parental_leave_clean |>
+summary_leave_length <- df_parental_leave_clean |>
     group_by(income_group) |>
-    filter(year > 2015) |>
+    filter(year == 2024) |>
     summarise(
-        "mean_paternityleave" = mean(paternityleave_lenght),
-        "mean_maternityleave" = mean(maternityleave_lenght),
-        "mean_parentalleave" = mean(shared_lenght)
+        "mean_maternityleave" = mean(maternityleave_length),
+        "mean_paternityleave" = mean(paternityleave_length),
+        "mean_parentalleave" = mean(shared_length)
+    ) |>
+    arrange(desc(mean_maternityleave))
+
+View(summary_leave_length)
+
+# average length of parental leaves by year
+
+summary_leave_length_yearly <- df_parental_leave_clean |>
+    group_by(year) |>
+    summarise(
+        "mean_maternityleave" = mean(maternityleave_length),
+        "mean_paternityleave" = mean(paternityleave_length),
+        "mean_parentalleave" = mean(shared_length)
     )
 
-View(summary_leave_lenght)
+
+View(summary_leave_length_yearly)
 
 # which countries have the longest leaves in 2024?
 
-df_parental_leave_clean |>
+maternity_2024 <- df_parental_leave_clean |>
     group_by(country) |>
     filter(year == 2024) |>
-    arrange(desc(maternityleave_lenght)) |>
+    arrange(desc(maternityleave_length))
+
+maternity_2024 |>
+    head()
+
+paternity_2024 <- df_parental_leave_clean |>
+    group_by(country) |>
+    filter(year == 2024) |>
+    arrange(desc(paternityleave_length))
+    
+paternity_2024 |>    
     head(5)
 
+shared_2024 <- df_parental_leave_clean |>
+    group_by(country) |>
+    filter(year == 2024) |>
+    arrange(desc(shared_length))
+
+shared_2024 |>
+    head(5)
 
 
 # How many countries had paternity leave each year?
@@ -105,13 +142,8 @@ df_parental_leave_clean |>
 paternity_yearly <- df_parental_leave_clean |>
     group_by(year) |>
     summarise(
-        "countries_with_paternityleave" = sum(paternityleave_log == 1)
+        "Countries with paternity leave" = sum(paternityleave_log == 1)
     )
-    
-# What was the average lenght of paternity leave in each year?
 
-paternity_yearly$leave_lenght <- df_parental_leave_clean |>
-    group_by(year) |>
-    summarise(
-        "mean_paternityleave" = mean(paternityleave_lenght)
-    )
+View(paternity_yearly)
+
--- animate.R
+++ animate.R
@@ -2,14 +2,14 @@ install.packages("gifski")
 library(gganimate)
 library(gifski)
 
-anim_plot <- ggplot(df_parental_leave_clean, aes(maternityleave_lenght, paternityleave_lenght, colour = country)) +
+anim_plot <- ggplot(df_parental_leave_clean, aes(maternityleave_length, paternityleave_length, colour = country)) +
     geom_point(size = 3, alpha = 0.7, show.legend = FALSE) +
     scale_colour_manual(values = country_colors) +
     scale_size(range = c(2, 60)) +
     facet_wrap(~region) +
     # Here comes the gganimate specific bits
     shadow_trail(max_frames = 5) +
-    labs(title = 'Year: {frame_time}', x = 'Lenght of maternity leave', y = 'Lenght of paternity leave') +
+    labs(title = 'Year: {frame_time}', x = 'length of maternity leave', y = 'length of paternity leave') +
     transition_time(as.integer(year)) +
     ease_aes('linear') +
     xlim(0, 200) + 
